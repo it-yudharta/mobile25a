f2a6bb3b917062faa0100be3d0ea18ea2e4c41c6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2025-03-24 15:22:45 +0700

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile25a/pages/home_page.dart';
+import 'package:mobile25a/pages/profile_page.dart';
+import 'package:mobile25a/pages/setting_page.dart';
 
 void main() => runApp(const MainApp());
 
@@ -7,67 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: MyWidget(label: "Cahya Bagus Sanjaya")),
-      ),
-    );
-  }
-}
-
-typedef ValueChanged<T> = void Function(T value);
-
-class MyWidget extends StatelessWidget {
-  final String label;
-  const MyWidget({super.key, required this.label});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [CounterWidget(init: 5), CounterWidget(init: 9)],
-        ),
-      ],
-    );
-  }
-}
-
-class CounterWidget extends StatefulWidget {
-  final int init;
-
-  const CounterWidget({super.key, required this.init});
-
-  @override
-  State<StatefulWidget> createState() => _CounterWidgetState(init);
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  int _counter = 0;
-
-  _CounterWidgetState(int init) {
-    _counter = init;
-  }
-
-  void _increment() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('$_counter'),
-          Text('$_counter'),
-          ElevatedButton(onPressed: _increment, child: const Text('Tambah')),
-        ],
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/setting': (context) => SettingPage(),
+        '/profile': (context) => ProfilePage(),
+      },
+      // home: Scaffold(body: Center(child: Text("halo"))),
     );
   }
 }
