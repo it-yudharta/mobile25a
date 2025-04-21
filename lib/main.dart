de014581da54575dc754e1c5df99bcb0f32657d7
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2025-04-21 14:55:30 +0700

    initial supabase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:mobile25a/pages/album_page.dart';
 import 'package:mobile25a/pages/home_page.dart';
 import 'package:mobile25a/pages/profile_page.dart';
 import 'package:mobile25a/pages/setting_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const MainApp());
+const String supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5bnlzaWxkc2RtanRxaGppbm90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMTk0MTQsImV4cCI6MjA2MDc5NTQxNH0.5nMAx-p7jmulMzm8wW5HuSkryh7c0ZnFbMgHuAQjgzQ';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://gynysildsdmjtqhjinot.supabase.co',
+    anonKey: supabaseKey,
+  );
+
+  runApp(const MainApp());
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
