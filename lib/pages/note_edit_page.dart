e3eb1239932337651f685e0f9a7e22e70a95630b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2025-04-28 14:19:23 +0700

    create notes

--- lib/pages/note_edit_page.dart
+++ lib/pages/note_edit_page.dart
@@ -1,13 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class NoteEditPage extends StatelessWidget {
+class NoteEditPage extends StatefulWidget {
   const NoteEditPage({super.key});
+
+  @override
+  State<NoteEditPage> createState() => _NoteEditPageState();
+}
+
+class _NoteEditPageState extends State<NoteEditPage> {
+  final _formKey = GlobalKey<FormState>();
+  String title = '';
+  String description = '';
+
+  Future save() async {
+    if (_formKey.currentState!.validate()) {
+      final supabase = Supabase.instance.client;
+
+      // Simpan catatan ke database
+      await supabase.from('notes').insert({
+        'title': title,
+        'description': description,
+      });
+
+      Navigator.pop(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Buat Catatan Baru")),
-      body: Center(child: Text("Buat Catatan Baru")),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Judul'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Judul tidak boleh kosong';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  title = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Deskripsi'),
+              onChanged: (value) {
+                setState(() {
+                  description = value;
+                });
+              },
+            ),
+            ElevatedButton(onPressed: save, child: const Text('Simpan')),
+          ],
+        ),
+      ),
     );
   }
 }
